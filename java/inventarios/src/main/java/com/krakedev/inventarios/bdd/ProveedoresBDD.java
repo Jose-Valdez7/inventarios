5400944d8c7a8fe75962d42ac442ce031df06b78
Jose-Valdez7 <joxzsian@gmail.com>
2024-05-01 20:38:25 -0500

    Agreagado insertarProveedor

--- java/inventarios/src/main/java/com/krakedev/inventarios/bdd/ProveedoresBDD.java
+++ java/inventarios/src/main/java/com/krakedev/inventarios/bdd/ProveedoresBDD.java
@@ -20,20 +20,24 @@ public class ProveedoresBDD {
 		Proveedor proveedor=null;
 		try {
 			con=ConexionBDD.obtenerConexion();
-			ps=con.prepareStatement("select identificador,tipo_documento,nombre,telefono,correo,direccion "
-					+ "from proveedores "
-					+ "where upper(nombre) like ?");
+			ps=con.prepareStatement("select prov.identificador,prov.tipo_documento,td.descripcion,prov.nombre,prov.telefono,prov.correo,prov.direccion "
+					+ "from proveedores prov,tipo_documentos td "
+					+ "where prov.tipo_documento=td.codigo "
+					+ "and upper(nombre) like ?");
 			ps.setString(1, "%"+subcadena.toUpperCase()+"%");
 			rs=ps.executeQuery();
 			
 			while(rs.next()) {
 				String identificador=rs.getString("identificador");
-				String tipoDocumento=rs.getString("tipo_documento");
+				String codigoTipoDocumento=rs.getString("tipo_documento");
+				String descripcionTD=rs.getString("descripcion");
 				String nombre=rs.getString("nombre");
 				String telefono=rs.getString("telefono");
 				String correo=rs.getString("correo");
 				String direccion=rs.getString("direccion"); 
-				proveedor=new Proveedor(identificador,tipoDocumento,nombre,telefono,correo,direccion);
+				TipoDocumentos td=new TipoDocumentos(codigoTipoDocumento,descripcionTD);
+				
+				proveedor=new Proveedor(identificador,td,nombre,telefono,correo,direccion);
 				proveedores.add(proveedor);
 			}
 			
@@ -75,5 +79,35 @@ public class ProveedoresBDD {
 			}
 			
 			return tipoDocumentos;
+		}
+		
+		public void insertar(Proveedor proveedor) throws KrakeDevException {
+			Connection con = null;
+			try {
+				con=ConexionBDD.obtenerConexion();
+				PreparedStatement ps = con.prepareStatement("insert into proveedores(identificador,tipo_documento,nombre,telefono,correo,direccion) "
+						+ "values (?,?,?,?,?,?)");
+				ps.setString(1, proveedor.getIdentificador());
+				ps.setString(2, proveedor.getTipoDocumento().getCodigo());
+				ps.setString(3, proveedor.getNombre());
+				ps.setString(4, proveedor.getTelefono());
+				ps.setString(5, proveedor.getCorreo());
+				ps.setString(6, proveedor.getDireccion());
+				
+				ps.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new KrakeDevException("Error al insertar el proveedores. Detalle:"+e.getMessage());
+			} catch (KrakeDevException e) {
+				throw e;
+			}finally {
+				if(con!=null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				}
+			}
 		}
 }
