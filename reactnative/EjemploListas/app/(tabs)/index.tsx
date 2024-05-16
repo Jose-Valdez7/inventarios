13c3b3ff6256afeabf0ee0d460476cc9cf616915
Jose-Valdez7 <joxzsian@gmail.com>
2024-05-16 02:32:40 -0500

    Agregado video Administrar Productos

---
+++ reactnative/EjemploListas/app/(tabs)/index.tsx
@@ -0,0 +1,222 @@
+import { Image, StyleSheet, Text, FlatList, View, Button, TextInput, Alert } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { useState } from 'react'
+
+
+let personas = [
+  { nombre: 'Thor', apellido: 'Thillas', cedula: '0938271625' },
+  { nombre: 'Amber', apellido: 'Flores', cedula: '1728390415' },
+  { nombre: 'Peter', apellido: 'Parker', cedula: '1309873651' },
+]
+
+//esNuevo indica si se esta creando una nueva persona o se esta modificando una existente
+let esNuevo=true;
+//indiceSeleccionado la variable almacena el indice del arreglos del elemento selecccionado para la edicion
+let indiceSeleccionado= -1;
+
+export default function HomeScreen() {
+  const [txtNombre, setTxtNombre] = useState("");
+  const [txtApellido, setTxtApellido] = useState("");
+  const [txtCedula, setTxtCedula] = useState("");
+  const [numElementos,setNumElementos]=useState(personas.length);
+
+  let ItemPersona = (props) => {
+    return (<View style={styles.itemPersona}>
+      <View style={styles.itemIndice}><Text style={styles.textoPrincipal}> {props.indice}</Text></View>
+      <View style={styles.itemContenido}><Text style={styles.textoPrincipal}> {props.persona.nombre} {props.persona.apellido}</Text>
+        <Text style={styles.textoSecundario}> {props.persona.cedula}</Text></View>
+      <View style={styles.itemBotones}>
+        <Button
+          title=' E '
+          color='green'
+          onPress={()=>{
+            setTxtCedula(props.persona.cedula);
+            setTxtNombre(props.persona.nombre);
+            setTxtApellido(props.persona.apellido);
+            esNuevo=false;
+            indiceSeleccionado=props.indice;
+          }}
+        />
+        <Button
+          title=' X '
+          color='red'
+          onPress={()=>{
+            indiceSeleccionado=props.indice;
+            personas.splice(indiceSeleccionado,1);
+            //splice funcion para borrar cosas del arreglo
+            //recibe 2 parametros desde que posicion se va a borrar
+            // segundo cuantos voy a borrar
+            console.log("ARREGLO PERSONAS", personas);
+            setNumElementos(personas.length);
+          }}
+        />
+  
+      </View>
+    </View>);
+  }
+
+  let limpiar = () => {
+    setTxtNombre("");
+    setTxtApellido("");
+    setTxtCedula("");
+    esNuevo=true;
+  }
+
+  let existePersona=()=>{
+    for(let i=0;i<personas.length;i++){
+      if(personas[i].cedula==txtCedula){
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  let guardarPersona = () => {
+    if(esNuevo){
+    if(existePersona()){
+      Alert.alert("INFO","Ya existe una personas con la cedula "+txtCedula);
+    }else{
+      let persona = { nombre: txtNombre, apellido: txtApellido, cedula: txtCedula };
+    personas.push(persona);
+    }
+    }else{
+      personas[indiceSeleccionado].nombre=txtNombre;
+      personas[indiceSeleccionado].apellido=txtApellido;
+    }
+    limpiar();
+  }
+
+  return (
+    <ThemedView style={styles.stepContainer}>
+      <View style={styles.areaCabecera}><ThemedText type='title' >PERSONAS 1</ThemedText>
+        <TextInput
+          style={styles.txt}
+          value={txtCedula}
+          placeholder='Ingrese su Cedula'
+          onChangeText={setTxtCedula}
+          keyboardType='numeric'
+          editable={esNuevo}
+        />
+        <TextInput
+          style={styles.txt}
+          value={txtNombre}
+          placeholder='Ingrese su Nombre'
+          onChangeText={setTxtNombre}
+        />
+        <TextInput
+          style={styles.txt}
+          value={txtApellido}
+          placeholder='Ingrese su Apellido'
+          onChangeText={setTxtApellido}
+        />
+        <View style={styles.areaBotones}>
+          <Button
+            title='Guardar'
+            onPress={() => {
+              guardarPersona();
+              setNumElementos(personas.length);
+            }}
+          />
+          <Button
+            title='Nuevo'
+            onPress={()=>{
+              limpiar();
+            }}
+          />
+        </View>
+        <Text>Elementos: {numElementos}</Text>
+      </View>
+      <View style={styles.areaContenido}>
+        <FlatList
+          style={styles.lista}
+          data={personas}
+          renderItem={(obj) => {
+            return <ItemPersona indice={obj.index} persona={obj.item}
+            />
+          }}
+          keyExtractor={(item) => {
+            return item.cedula;
+          }}
+        /></View>
+      <View style={styles.areaPie}><Text> Autor: Jose Valdez</Text></View>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  stepContainer: {
+    flex: 1,
+    //backgroundColor: 'lightblue',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    paddingHorizontal: 10,
+    paddingTop: 50,
+  },
+  lista: {
+    //backgroundColor: 'lightpink',
+  },
+  itemPersona: {
+    flexDirection:'row',
+    backgroundColor: "navy",
+    marginBottom: 10,
+    padding: 10,
+
+  },
+  textoPrincipal: {
+    fontSize: 20,
+    color: 'white'
+  },
+  textoSecundario: {
+    fontStyle: 'italic',
+    fontSize: 16,
+    color: 'yellow'
+  },
+  areaCabecera: {
+    flex: 4,
+    //backgroundColor: 'Chocolate',
+    justifyContent: 'center',
+  },
+  areaContenido: {
+    flex: 6,
+    //backgroundColor: "cadetblue"
+  },
+  areaPie: {
+    flex: 1,
+    //backgroundColor: "orange",
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  itemIndice: {
+    flex: 1,
+    //backgroundColor: 'darkgray',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itemContenido: {
+    flex: 6,
+    //backgroundColor: 'darkorange',
+    paddingLeft: 5,
+  },
+  itemBotones: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  txt: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    paddingTop: 3,
+    paddingHorizontal: 5,
+    marginBottom: 5
+  },
+  areaBotones: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+
+  }
+});
---
+++ reactnative/Productos/app/(tabs)/index.tsx
@@ -0,0 +1,265 @@
+import { StyleSheet, Text, FlatList, View, Button, TextInput, Alert,ScrollView } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { useState,useEffect } from 'react';
+
+let productos = [
+  { nombre: 'Doritos', categoria: 'Snacks', precioCompra: 0.40, precioVenta: 0.45, id: 100 },
+  { nombre: 'Manicho', categoria: 'Golosinas', precioCompra: 0.20, precioVenta: 0.25, id: 101 },
+  { nombre: 'Rufles', categoria: 'Snacks', precioCompra: 0.45, precioVenta: 0.60, id: 102 },
+  { nombre: 'Trululu', categoria: 'Gomitas', precioCompra: 0.60, precioVenta: 0.75, id: 103 },
+  { nombre: 'Ferrero', categoria: 'Golosinas', precioCompra: 0.95, precioVenta: 1.25, id: 104 },
+]
+
+let esNuevo = true;
+let idSeleccionado = -1;
+
+export default function HomeScreen() {
+  const [txtNombre, setTxtNombre] = useState("");
+  const [txtCategoria, setTxtCategoria] = useState("");
+  const [txtPrecioCompra, setTxtPrecioCompra] = useState("");
+  const [txtPrecioVenta, setTxtPrecioVenta] = useState("");
+  const [txtId, setTxtId] = useState("");
+  const [numElementos, setNumElementos] = useState(productos.length);
+
+  useEffect(() => {
+    if (txtPrecioCompra !== "") {
+      const nuevoPrecioVenta = (parseFloat(txtPrecioCompra) * 0.20 + parseFloat(txtPrecioCompra)).toFixed(2);
+      setTxtPrecioVenta(nuevoPrecioVenta.toString());
+    }
+  }, [txtPrecioCompra]);
+
+  let ItemProducto = (props) => {
+    return (<View style={styles.itemProducto}>
+      <View style={styles.itemIndice}><Text style={styles.textoPrincipal}> {props.producto.id}</Text></View>
+      <View style={styles.itemContenido}><Text style={styles.textoPrincipal}> {props.producto.nombre} </Text>
+        <Text style={styles.textoSecundario}> {props.producto.categoria} </Text></View>
+      <View style={styles.itemPrecio}><Text style={styles.textoPrincipal}> {props.producto.precioVenta} </Text></View>
+      <View style={styles.itemBotones}>
+        <Button
+          title=' E '
+          color='green'
+          onPress={() => {
+            setTxtCategoria(props.producto.categoria);
+            setTxtNombre(props.producto.nombre);
+            setTxtPrecioCompra(props.producto.precioCompra);
+            setTxtPrecioVenta(props.producto.precioVenta);
+            esNuevo = false;
+            idSeleccionado = props.indice;
+          }}
+        />
+        <Button
+          title=' X '
+          color='red'
+          onPress={() => {
+            idSeleccionado = props.indice;
+            productos.splice(idSeleccionado, 1);
+            console.log("ARREGLO PRODUCTOS", productos);
+            setNumElementos(productos.length);
+          }}
+        />
+
+      </View>
+    </View>);
+  }
+
+  let limpiar = () => {
+    setTxtNombre("");
+    setTxtCategoria("");
+    setTxtPrecioCompra("");
+    setTxtId("");
+    setTxtPrecioVenta("");
+    esNuevo = true;
+  }
+
+  let guardarProducto = () => {
+    if (esNuevo) {
+      if (txtId == "" || txtNombre == "" || txtCategoria == "" || txtPrecioCompra == "" || txtPrecioVenta == "") {
+        Alert.alert("INFO", "Debe llenar todos los campos ");
+      } else {
+        let producto = { nombre: txtNombre, categoria: txtCategoria, precioCompra: parseFloat(txtPrecioCompra), precioVenta: parseFloat(txtPrecioVenta), id: parseFloat(txtId) };
+        productos.push(producto);
+        limpiar();
+      }
+    } else {
+      if (txtNombre == "" || txtCategoria == "" || txtPrecioCompra == "" || txtPrecioVenta == "") {
+        Alert.alert("INFO", "Debe llenar todos los campos");
+      } else {
+        productos[idSeleccionado].nombre = txtNombre;
+        productos[idSeleccionado].categoria = txtCategoria;
+        productos[idSeleccionado].precioCompra = parseFloat(txtPrecioCompra);
+        productos[idSeleccionado].precioVenta = parseFloat((parseFloat(txtPrecioCompra) * 0.20 + parseFloat(txtPrecioCompra)).toFixed(2));
+        limpiar();
+      }
+    }
+  }
+  return (
+
+    <ThemedView style={styles.stepContainer}>
+      <View style={styles.areaCabecera}><ThemedText type='title' style={styles.titulo}>PRODUCTOS </ThemedText>
+        <ScrollView contentContainerStyle={styles.scrollView}>
+        <View>
+        <TextInput
+          style={styles.txt}
+          value={txtId}
+          placeholder='CODIGO'
+          onChangeText={setTxtId}
+          keyboardType='numeric'
+          editable={esNuevo}
+        />
+        <TextInput
+          style={styles.txt}
+          value={txtNombre}
+          placeholder='NOMBRE'
+          onChangeText={setTxtNombre}
+        />
+        <TextInput
+          style={styles.txt}
+          value={txtCategoria}
+          placeholder='CATEGORIA'
+          onChangeText={setTxtCategoria}
+        />
+        <TextInput
+          style={styles.txt}
+          value={txtPrecioCompra}
+          placeholder='PRECIO DE COMPRA'
+          keyboardType='decimal-pad'
+          onChangeText={setTxtPrecioCompra}
+        />
+        <TextInput
+          style={styles.txt}
+          placeholder='PRECIO DE VENTA'
+          value={txtPrecioVenta }
+          keyboardType='decimal-pad'
+          onChangeText={setTxtPrecioVenta}
+          editable={false}
+        />
+        <View style={styles.areaBotones}>
+          <Button
+            title='Guardar'
+            onPress={() => {
+              guardarProducto();  
+              setNumElementos(productos.length);
+            }}
+          />
+          <Button
+            title='Nuevo'
+            onPress={() => {
+              limpiar();
+            }}
+          />
+          <Text>Productos: {numElementos}</Text>
+        </View></View>
+        </ScrollView>
+      </View>
+      <FlatList
+        style={styles.areaContenido}
+        data={productos}
+        renderItem={(obj) => {
+          return <ItemProducto
+            producto={obj.item} indice={obj.index}
+          />
+        }}
+        keyExtractor={(item) => {
+          return item.id.toString();
+        }}
+      />
+      <View style={styles.areaPie}><Text> Autor: Jose Valdez</Text></View>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  stepContainer: {
+    flex: 1,
+    //backgroundColor:'blue',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    paddingHorizontal: 10,
+    paddingTop: 50,
+  },
+  titulo: {
+    flex: 0,
+    marginHorizontal: 65,
+    alignItems: 'center',
+    padding: 10,
+  },
+  itemProducto: {
+    flex:5,
+    flexDirection:'row',
+    backgroundColor: "navy",
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 10,
+    borderColor: 'red',
+    borderWidth: 2,
+  },
+  textoPrincipal: {
+    fontSize: 20,
+    color: 'white'
+  },
+  textoSecundario: {
+    fontStyle: 'italic',
+    fontSize: 16,
+    color: 'yellow'
+  },
+  itemIndice: {
+    flex: 2,
+    //backgroundColor: 'darkgray',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itemContenido: {
+    flex: 6,
+    //backgroundColor: 'darkorange',
+    paddingLeft: 5,
+  },
+  itemPrecio: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  itemBotones: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  txt: {
+    //backgroundColor: 'darkgray',
+    borderWidth: 1,
+    borderColor: 'gray',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    borderRadius:6,
+    width:'100%'
+  },
+  areaBotones: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding:8,
+    alignItems:'center',
+  },
+  areaCabecera: {
+    flex: 0,
+    backgroundColor: 'Chocolate',
+    justifyContent: 'center',
+  },
+  areaContenido: {
+    flex: 16,
+    //backgroundColor: "cadetblue"
+  },
+  areaPie: {
+    flex: 0,
+    //backgroundColor: "orange",
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  scrollView: {
+    flexGrow: 1,
+    //backgroundColor:"red"
+  },
+});
